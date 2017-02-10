dc420fee721796295b39edcf3ee57e6bfbb1d6a8
cickib <bellakocsis06@gmail.com>
2017-02-10 10:23:26 +0100

    LSD-14 Repository interface annotations added

--- src/main/java/com/codecool/repository/UserRepository.java
+++ src/main/java/com/codecool/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.codecool.repository;
 
 import com.codecool.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface UserRepository extends JpaRepository {
+@Repository
+interface UserRepository extends JpaRepository<User, Integer> {
     User findByEmail(String email);
 }
