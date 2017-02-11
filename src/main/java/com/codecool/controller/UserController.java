f28d42956df37d223f0551666378c7c38f9f3aca
berloc <berloc1987@gmail.com>
2017-02-11 12:24:18 +0100

    LSD-12 Route in Controller basic version of saving the data into the db (just for testing)

--- src/main/java/com/codecool/controller/UserController.java
+++ src/main/java/com/codecool/controller/UserController.java
@@ -1,22 +1,47 @@
 package com.codecool.controller;
 
 
+import com.codecool.model.User;
+import com.codecool.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String index() {
-        return "index";
+        return "testindex";
     }
 
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration() {
-        return "csáá";
+    @ResponseBody
+    public String registration(@RequestBody String data) {
+
+
+//        JSON from String to Object
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            User user = mapper.readValue(data, User.class);
+            userRepository.save(user);
+        } catch (JsonGenerationException | JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "gut";
     }
 }
