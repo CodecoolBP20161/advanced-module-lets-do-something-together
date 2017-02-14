6d743d81df28b2ad3f3f4c9e9a096094e70f8779
Lőrincz Szilvia <lorszil@Lorinczs-MacBook-Pro.local>
2017-02-14 10:06:08 +0100

    making simplier sending json

--- src/main/resources/static/js/controller/user_controller.js
+++ src/main/resources/static/js/controller/user_controller.js
@@ -1,25 +1,25 @@
+'use strict';
 
-var helloApp = angular.module("helloApp", ['ngResource']);
-helloApp.config(['$httpProvider', function ($httpProvider) {
+var actimate = angular.module("actimate", ['ngResource']);
+actimate.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 }]);
-helloApp.controller("UserCtrl", function($scope, $http) {
-    $scope.users = [
-
-    ];
+actimate.controller("UserCtrl", function($scope, $http) {
+    $scope.user = {}
+    ;
+    console.log($scope.user);
     $scope.addRow = function(){
-        $scope.users.push({ 'email':$scope.email, 'password': $scope.password, 'password_again':$scope.password_again });
+        /*$scope.users.push({ 'email':$scope.email, 'password': $scope.password, 'password_again':$scope.password_again });
         $scope.email='';
         $scope.password='';
         $scope.password_again='';
-
-        var data = 'email=' + $scope.email + '&password=' + $scope.password;
-        $http.post('/registration', data )
+*/
+        $http.post('/registration', $scope.user)
             .success(function(data, status, headers, config) {
                 $scope.message = data;
             })
             .error(function(data, status, headers, config) {
-                alert( "failure message: " + JSON.stringify(data));
+                alert( "failure message: " + $scope.user);
             });
         // Making the fields empty
         //
@@ -27,4 +27,5 @@ helloApp.controller("UserCtrl", function($scope, $http) {
         $scope.password='';
         $scope.password_again='';
     };
+
     });
