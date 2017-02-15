f5fce64c51a5fe8a740663dd44ec282ed22b9480
cickib <bellakocsis06@gmail.com>
2017-02-15 16:52:56 +0100

    LSD-2 - Role simplified

--- src/main/java/com/codecool/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.repository;
-
-import com.codecool.security.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {}
--- src/main/java/com/codecool/security/Role.java
+++ src/main/java/com/codecool/security/Role.java
@@ -1,37 +1,5 @@
 package com.codecool.security;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
-@Entity
-@Table(name = "roles")
 public enum Role {
-    USER, ADMIN;
-
-    private int id;
-    private String role;
-
-    Role() {}
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", unique = true)
-    @NotNull
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Column(name = "role")
-    @NotNull
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    USER, ADMIN
 }
