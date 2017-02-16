e4c45c78ded8b5c9bcbeff3f968867c759464092
lorszil <lorszil@gmail.com>
2017-02-16 22:46:18 +0100

    comperTo is not in the user_controller

--- src/main/resources/static/js/controller/user_controller.js
+++ src/main/resources/static/js/controller/user_controller.js
@@ -28,24 +28,4 @@ actimate.controller("UserCtrl", function($scope, $http) {
         // Making the fields empty
         $scope.user = null;
     };
-
-    $scope.compareTo = function(){
-        return {
-            require: "ngModel",
-            scope: {
-                otherModelValue: "=compareTo"
-            },
-            link: function(scope, element, attributes, ngModel) {
-
-                ngModel.$validators.compareTo = function(modelValue) {
-                    return modelValue == scope.otherModelValue;
-                };
-
-                scope.$watch("otherModelValue", function() {
-                    ngModel.$validate();
-                });
-            }
-        };
-    };
-    actimate.directive("compareTo", compareTo);
 });
