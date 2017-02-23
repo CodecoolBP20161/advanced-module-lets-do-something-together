da42d11764a2b469e13e2f9004fb04a44d0bc82e
cickib <bellakocsis06@gmail.com>
2017-02-23 20:38:43 +0100

    LSD-47 - scheduled tasks implemented - called every 30000 milliseconds

--- src/main/java/com/codecool/controller/AppEmailController.java
+++ src/main/java/com/codecool/controller/AppEmailController.java
@@ -1,13 +1,17 @@
 package com.codecool.controller;
 
+import com.codecool.email.EmailHandler;
 import com.codecool.model.User;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,12 +21,19 @@ public class AppEmailController {
 
     private static final String URL = "http://localhost:60227";
 
-    public static void builderSend(List<User> users) {
+    @Autowired
+    private static EmailHandler emailHandler;
+
+    @Autowired
+    public AppEmailController(EmailHandler handler) {
+        emailHandler = handler;
+    }
+
+    public static void builderSend(String users) {
         URIBuilder builder;
         try {
             builder = new URIBuilder(URL);
-            List<String> emails = users.stream().map(User::getEmail).collect(Collectors.toList());
-            builder.addParameter("emails", String.join(",", emails));
+            builder.addParameter("emails", users);
             execute(builder.build());
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
@@ -43,5 +54,23 @@ public class AppEmailController {
 
     private static String execute(URI uri) throws IOException, URISyntaxException {
         return Request.Get(uri).execute().returnContent().asString();
+    }
+
+    @Scheduled(fixedDelayString = "30000")
+    private void manageNewRegistrations() {
+        List<String> emails = emailHandler
+                .checkEmailStatus()
+                .stream()
+                .map(User::getEmail)
+                .collect(Collectors.toList());
+        builderSend(String.join(",", emails));
+    }
+
+    @Scheduled(fixedDelayString = "30001")
+    private void manageSentEmails() {
+        List<String> sentEmails = Arrays.asList(builderGet().split(","));
+        if (sentEmails.size() > 1) {
+            emailHandler.updateEmailStatus(sentEmails);
+        }
     }
 }
