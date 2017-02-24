0c747c97b5700a19272d4465f20aad3234eae295
cickib <bellakocsis06@gmail.com>
2017-02-24 11:17:43 +0100

    LSD-54 User model updated with userDetail field (foreign key)

--- src/main/java/com/codecool/model/User.java
+++ src/main/java/com/codecool/model/User.java
@@ -2,66 +2,42 @@ package com.codecool.model;
 
 
 import com.codecool.security.Role;
+import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "`user`")
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(unique = true)
+    @NotNull
     private Integer id;
+
+    @NotNull
+    @Column(unique = true)
     private String email;
+
+    @NotNull
     private String password;
+
+    @NotNull
+    @Enumerated(value = EnumType.STRING)
     private Role role;
+
+    @OneToOne
+    @JoinColumn(name = "userDetail")
+    private UserDetail userDetail;
 
     public User() {
     }
 
     public User(String email, String password) {
         this.email = email;
-        this.password = password;
-    }
-
-    @Column(name = "role", nullable = false)
-    @Enumerated(value = EnumType.STRING)
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", unique = true)
-    @NotNull
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @NotNull
-    @Column(name = "email", unique = true)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @NotNull
-    @Column(name = "password")
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
