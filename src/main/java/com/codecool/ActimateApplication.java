23a293ca9af78e82a54f3400ae1e8615c9274987
cickib <bellakocsis06@gmail.com>
2017-03-05 13:39:54 +0100

    LSD-60 - initial data loaded in separate class

--- src/main/java/com/codecool/ActimateApplication.java
+++ src/main/java/com/codecool/ActimateApplication.java
@@ -1,10 +1,13 @@
 package com.codecool;
 
+import com.codecool.config.DataLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActimateApplication {
+
+    private DataLoader dataLoader;
 
     public static void main(String[] args) {
         SpringApplication.run(ActimateApplication.class, args);
--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,9 +1,5 @@
 package com.codecool.controller;
 
-import com.codecool.model.Interest;
-import com.codecool.model.User;
-import com.codecool.repository.InterestRepository;
-import com.codecool.security.Role;
 import com.codecool.security.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,36 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 @Controller
 public class MainController {
-
-    private List<String> basicActivities = new ArrayList<>(
-            Arrays.asList("tennis", "gokart", "running", "cardGames",
-                    "cinema", "theater", "cityWalks", "hiking"));
-
-    @Autowired
-    private InterestRepository interestRepository;
 
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = {"/", "logout"}, method = RequestMethod.GET)
-    public String index(Model model, String logout) {
-//        TODO delete this
-//        default admin user to test login/access/etc
-        if (userService.getUserByEmail("admin@admin.com").equals(Optional.empty()) ||
-                userService.getUserByEmail("admin@admin.com") == null) {
-            User admin = new User("admin@admin.com", "1234");
-            userService.create(admin, Role.ADMIN);
-        }
-        if (interestRepository.findAll().isEmpty()) {
-            basicActivities.forEach(activity -> interestRepository.save(new Interest(activity)));
-        }
+    public String index(String logout) {
         return "main";
     }
 
