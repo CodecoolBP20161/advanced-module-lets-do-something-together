7aa3a5638f08bab301cb805a004622d7a40cb615
berloc <berloc1987@gmail.com>
2017-03-08 13:56:28 +0100

    LSD-77 fixed annotation fuckedup

--- src/main/java/com/codecool/repository/EventRepository.java
+++ src/main/java/com/codecool/repository/EventRepository.java
@@ -3,10 +3,11 @@ package com.codecool.repository;
 import com.codecool.model.Event;
 import com.codecool.model.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface EventRepository extends JpaRepository {
 
     List<Event> findByDate(String date);
