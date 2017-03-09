2463c7be6326e1bc4b6ddfa699b53411883fb18b
cickib <bellakocsis06@gmail.com>
2017-03-09 11:55:12 +0100

    LSD-82 - interests returned in json

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,12 +1,23 @@
 package com.codecool.controller;
 
+import com.codecool.model.Interest;
+import com.codecool.repository.InterestRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private InterestRepository interestRepository;
 
     @RequestMapping(value = {"/", "logout"}, method = RequestMethod.GET)
     public String index() {
@@ -29,5 +40,14 @@ public class MainController {
     @RequestMapping(value = "/mission", method = RequestMethod.GET)
     public String mission() {
         return "mission";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/interests", method = RequestMethod.GET)
+    public String interests() throws JSONException {
+        return new JSONObject()
+                .put("interests", interestRepository.findAll()
+                        .stream().map(Interest::getActivity)
+                        .collect(Collectors.toList())).toString();
     }
 }
