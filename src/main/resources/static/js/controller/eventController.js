14e22e027e33f785bc55e2b344f9c9d08a9f6ffd
DorciM <dora.medgyasszay@gmail.com>
2017-03-10 11:49:23 +0100

    all event data sent, inlcuding location coordinates

--- src/main/resources/static/js/controller/eventController.js
+++ src/main/resources/static/js/controller/eventController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var actimate = angular.module('actimate', ['ngResource']);
+var actimate = angular.module('actimate', ['ngResource', 'gm']);
 actimate.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -13,13 +13,20 @@ actimate.controller('saveEventCtrl', function ($scope, $http) {
 
         $http({
             method: 'POST',
-            url: '/createevent',
+            url: '/create-event',
             headers: {'Content-Type': 'application/json; charset=UTF-8'},
             data: JSON.stringify($scope.event)
         })
             .then(function (response) {
                 console.log($scope.event);
             })
-    }
+    };
+
+    $scope.$on('gmPlacesAutocomplete::placeChanged', function(){
+        var location = $scope.autocomplete.getPlace().geometry.location;
+        $scope.event.lat = location.lat();
+        $scope.event.lng = location.lng();
+        $scope.$apply();
+    });
     
 });
