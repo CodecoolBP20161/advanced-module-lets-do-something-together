d0cbb5aae7a0a92ec4fbde219419929e39e6ec19
berloc <berloc1987@gmail.com>
2017-03-13 13:17:07 +0100

    LSD-85 basic version of events page with filtering without error handling

--- src/main/java/com/codecool/controller/AdminController.java
+++ src/main/java/com/codecool/controller/AdminController.java
@@ -1,5 +1,10 @@
 package com.codecool.controller;
 
+import com.codecool.model.Interest;
+import com.codecool.model.event.Event;
+import com.codecool.model.event.Status;
+import com.codecool.repository.EventRepository;
+import com.codecool.repository.InterestRepository;
 import com.codecool.repository.UserEmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,12 +12,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
 @RequestMapping(value = "/admin")
 @Controller
 public class AdminController extends AbstractController {
 
     @Autowired
     private UserEmailRepository userEmailRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private InterestRepository interestRepository;
 
     @RequestMapping(method = RequestMethod.GET)
     public String mainUI() {
@@ -25,11 +39,38 @@ public class AdminController extends AbstractController {
         return "admin/admin_users";
     }
 
-    @RequestMapping(value = "/activities", method = RequestMethod.GET)
-    public String listActivities(Model model) {
-        model.addAttribute("activities", "NotImplementedError or whatevs");
-        return "admin/admin_activities";
+    @RequestMapping(value = "/events", method = RequestMethod.GET)
+    public String listEvents(Model model) {
+        model.addAttribute("events", eventRepository.findAll());
+        return "admin/admin_events";
     }
+
+     @RequestMapping(value = "/events", method = RequestMethod.POST)
+     public String listEvents(HttpServletRequest request, Model model) {
+         String key = request.getParameter("key").toLowerCase();
+         String filter = request.getParameter("filter");
+
+         switch (filter) {
+             case "status":
+                 if (key.equals("active")) {
+                     Status status = Status.ACTIVE;
+                     List<Event> events = eventRepository.findByStatus(status);
+                     model.addAttribute("events", events);
+                 } else {
+                     Status status = Status.PAST;
+                     List<Event> events = eventRepository.findByStatus(status);
+                     model.addAttribute("events", events);
+                 }
+             case "date":
+                 List<Event> events = eventRepository.findByDate(key);
+                 model.addAttribute("events", events);
+             case "activities":
+                 Interest interest = interestRepository.findByActivity(key);
+                 events = eventRepository.findByInterest(interest);
+                 model.addAttribute("events", events);
+         }
+         return "admin/admin_events";
+     }
 
     @RequestMapping(value = "/email", method = RequestMethod.GET)
     public String listUserWithUnsentEmails(Model model) {
--- src/main/java/com/codecool/repository/EventRepository.java
+++ src/main/java/com/codecool/repository/EventRepository.java
@@ -2,6 +2,7 @@ package com.codecool.repository;
 
 import com.codecool.model.Interest;
 import com.codecool.model.event.Event;
+import com.codecool.model.event.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +17,7 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
 
     List<Event> findByParticipants(Integer number);
 
-    List<Event> findByInterest(Interest category);
+    List<Event> findByInterest(Interest interest);
 
-    List<Event> findByStatus(String status);
+    List<Event> findByStatus(Status status);
 }
