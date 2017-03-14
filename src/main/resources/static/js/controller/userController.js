0762eba2fc530f947f62f8cf323d011405144e9b
lorszil <lorszil@gmail.com>
2017-03-14 10:57:27 +0100

    should work now, back to the station before refact

--- src/main/resources/static/js/controller/userController.js
+++ src/main/resources/static/js/controller/userController.js
@@ -1,37 +1,37 @@
 'use strict';
 
+var actimate = angular.module("actimate", ['ngResource']);
 actimate.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 }]);
 
-actimate.controller("UserCtrl",
-    function ($scope, $http) {
-        $scope.user = {};
-        $scope.errorConfirm = false;
+actimate.controller("UserCtrl", function ($scope, $http) {
+    $scope.user = {};
+    $scope.errorConfirm = false;
 
-        $scope.addUser = function (valid) {
-            if (valid) {
-                $http({
-                    method: 'POST',
-                    url: '/registration',
-                    //withCredentials:true,
-                    headers: {'Content-Type': 'application/json; charset=UTF-8'},
-                    data: angular.toJson($scope.user)
+    $scope.addUser = function (valid) {
+        if (valid) {
+            $http({
+                method: 'POST',
+                url: '/registration',
+                //withCredentials:true,
+                headers: {'Content-Type': 'application/json; charset=UTF-8'},
+                data: angular.toJson($scope.user)
+            })
+                .success(function (response) {
+                    if (response == "fail") {
+                        $(".email-error-message").text("Email address is already in use").addClass("alert alert-danger alert-dismissable").fadeIn();
+                        addError($("#emailDiv"));
+                    } else {
+                        onSuccess();
+                    }
+                    console.log(response);
                 })
-                    .success(function (response) {
-                        if (response == "fail") {
-                            $(".email-error-message").text("Email address is already in use").addClass("alert alert-danger alert-dismissable").fadeIn();
-                            addError($("#emailDiv"));
-                        } else {
-                            onSuccess();
-                        }
-                        console.log(response);
-                    })
-                    .error(function (response) {
-                        alert("failure message: " + angular.toJson($scope.user));
-                    });
-            }
-            // Making the fields empty
-            $scope.user = null;
-        };
-    });
+                .error(function (response) {
+                    alert("failure message: " + angular.toJson($scope.user));
+                });
+        }
+        // Making the fields empty
+        $scope.user = null;
+    };
+});
