be062347cc8a5f97e3711753e61a86725b4a86cc
cickib <bellakocsis06@gmail.com>
2017-03-15 10:36:28 +0100

    LSD-98 - event test - form rendered with logged in user

--- src/test/java/com/codecool/controller/EventControllerTest.java
+++ src/test/java/com/codecool/controller/EventControllerTest.java
@@ -3,6 +3,7 @@ package com.codecool.controller;
 import com.codecool.test.AbstractTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -10,8 +11,10 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -37,5 +40,16 @@ public class EventControllerTest extends AbstractTest {
     @Test
     public void createEventFormUnavailableWithoutLogin() throws Exception {
         mockMvc.perform(get("/u/create_event")).andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser
+    public void renderCreateEventFormTest() throws Exception {
+        mockMvc.perform(get("/u/create_event")).andExpect(status().is2xxSuccessful());
+
+        mockMvc.perform(get("/u/create_event"))
+                .andExpect(content().string(containsString(" Create Event")))
+                .andExpect(content().string(containsString("input type=\"text\"")))
+                .andExpect(content().string(containsString("Submit")));
     }
 }
