103112182836032d5c40f7d34bb3ec8195ac9519
cickib <bellakocsis06@gmail.com>
2017-03-15 11:17:27 +0100

    LSD-98 - event test - event created properly

--- src/test/java/com/codecool/controller/EventControllerTest.java
+++ src/test/java/com/codecool/controller/EventControllerTest.java
@@ -1,8 +1,14 @@
 package com.codecool.controller;
 
+import com.codecool.model.User;
+import com.codecool.repository.EventRepository;
+import com.codecool.security.Role;
+import com.codecool.security.service.user.UserService;
 import com.codecool.test.AbstractTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,8 +18,10 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.annotation.Resource;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +36,12 @@ public class EventControllerTest extends AbstractTest {
     @Resource
     private FilterChainProxy springSecurityFilterChain;
 
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private UserService userService;
+
     private String route;
 
     @Before
@@ -39,6 +53,7 @@ public class EventControllerTest extends AbstractTest {
                 .build();
 
         route = "/u/create_event";
+        userService.create(new User("user@user.com", "password"), Role.USER);
     }
 
     @Test
@@ -56,4 +71,27 @@ public class EventControllerTest extends AbstractTest {
                 .andExpect(content().string(containsString("input type=\"text\"")))
                 .andExpect(content().string(containsString("Submit")));
     }
+
+    @Test
+    @WithMockUser(username = "user@user.com")
+    public void createEventTest() throws Exception {
+        int eventsBefore = eventRepository.findAll().size();
+
+        mockMvc.perform(post(route)
+                .content("{\"name\":\"eventName\"," +
+                        "\"interest\":\"gokart\", " +
+                        "\"lng\":\"47.505013\", " +
+                        "\"lat\":\"19.057821\", " +
+                        "\"date\":\"2017-03-15T23:00:00.000Z\", " +
+                        "\"participants\":\"42\", \"description\":\"none\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).
+                andExpect(status().is2xxSuccessful());
+
+        int eventsAfter = eventRepository.findAll().size();
+
+        assertEquals(eventsBefore + 1, eventsAfter);
+        assertEquals(42, eventRepository.findAll().get(0).getParticipants());
+    }
+
 }
