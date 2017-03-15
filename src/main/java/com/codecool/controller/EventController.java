a42e11b33824efe2bd1b2b36e2be8ca3f701c44e
cickib <bellakocsis06@gmail.com>
2017-03-15 14:33:45 +0100

    LSD-72 - createEvent() simplified

--- src/main/java/com/codecool/controller/EventController.java
+++ src/main/java/com/codecool/controller/EventController.java
@@ -1,6 +1,5 @@
 package com.codecool.controller;
 
-import com.codecool.model.User;
 import com.codecool.model.event.Coordinates;
 import com.codecool.model.event.Event;
 import com.codecool.model.event.Status;
@@ -14,13 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.security.Principal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 
 @RequestMapping(value = "/u")
@@ -36,39 +36,23 @@ public class EventController extends AbstractController {
     }
 
     @RequestMapping(value = "/create_event", method = RequestMethod.POST)
-    public String createEvent(@RequestBody String data, Principal principal) throws JSONException, IOException, IllegalAccessException {
+    public String createEvent(@RequestBody String data, Principal principal) throws JSONException {
         JSONObject jsonData = new JSONObject(data);
         Event event = new Event();
-        for (Field field : getBasicEventFields()) {
-            field.setAccessible(true);
-            Object fieldValue = null;
-            if (field.getType().equals(Coordinates.class)) {
-                fieldValue = new Coordinates(jsonData.get("lng"), jsonData.get("lat"));
-            } else if (field.getType().equals(int.class)) {
-                fieldValue = Integer.parseInt(jsonData.get(field.getName()).toString());
-            } else if (field.getType().equals(Date.class)) {
-                DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-                try {
-                    fieldValue = format.parse(jsonData.get(field.getName()).toString());
-                } catch (ParseException e) {
-                    e.getMessage();
-                }
-            } else {
-                fieldValue = jsonData.get(field.getName());
-            }
-            field.set(event, fieldValue);
+        event.setParticipants(Integer.parseInt(jsonData.get("participants").toString()));
+        event.setDescription(jsonData.get("description").toString());
+        event.setName(jsonData.get("name").toString());
+        event.setLocation(new Coordinates(jsonData.get("lng"), jsonData.get("lat")));
+        try {
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            event.setDate(format.parse(jsonData.get("date").toString()));
+        } catch (ParseException e) {
+            e.getMessage();
         }
         event.setInterest(interestRepository.findByActivity(jsonData.get("interest").toString()));
-        User user = userService.getUserByEmail(principal.getName()).get();
-        event.setUser(user);
+        event.setUser(userService.getUserByEmail(principal.getName()).get());
         eventRepository.save(event);
         return "create_event";
-    }
-
-    //    foreign key fields handled separately
-    private List<Field> getBasicEventFields() {
-        Field[] fieldsArray = Event.class.getDeclaredFields();
-        return Arrays.asList(fieldsArray).subList(1, 6);
     }
 
     //    method called daily at midnight
