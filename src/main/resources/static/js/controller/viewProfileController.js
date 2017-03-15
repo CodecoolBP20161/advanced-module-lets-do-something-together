d6ca6f49add43c8453d30cef2d29672c0bcac48d
cickib <bellakocsis06@gmail.com>
2017-03-15 15:22:53 +0100

    LSD-37 - profile page issues fixed

--- src/main/resources/static/js/controller/viewProfileController.js
+++ src/main/resources/static/js/controller/viewProfileController.js
@@ -1,6 +1,11 @@
 'use strict';
 
-actimate.controller('loadUserCtrl', function($scope, $http){
+var actimate = angular.module("actimate", ['ngResource']);
+actimate.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+}]);
+
+actimate.controller('loadUserCtrl', function ($scope, $http) {
 
     $scope.user = null;
 
