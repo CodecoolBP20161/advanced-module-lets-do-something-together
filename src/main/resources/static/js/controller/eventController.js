a804fe8d339bc183cc289198e3673c3a1b67d0a2
DorciM <dora.medgyasszay@gmail.com>
2017-03-15 21:35:33 +0100

    angular validation with messages

--- src/main/resources/static/js/controller/eventController.js
+++ src/main/resources/static/js/controller/eventController.js
@@ -1,10 +1,17 @@
 'use strict';
 
-var actimate = angular.module('actimate', ['ngResource', 'gm']);
+var actimate = angular.module('actimate', ['ngResource', 'gm', 'jcs-autoValidate']);
 actimate.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
+actimate.run(function(defaultErrorMessageResolver){
+    defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+        errorMessages['wrongNumber'] = 'Please invite at least 1 person!';
+        errorMessages['wrongInputType'] = 'Please enter letters only!';
+    });
+}
+);
 
 actimate.controller('saveEventCtrl', function ($scope, $http) {
    $scope.event = {};
