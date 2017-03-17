6379467751f520f9a28ff80f46d5dfedc48e2715
cickib <bellakocsis06@gmail.com>
2017-03-17 11:51:07 +0100

    LSD-72 - DataLoader - unnecessary lines removed, UserDetail added

--- src/main/java/com/codecool/config/DataLoader.java
+++ src/main/java/com/codecool/config/DataLoader.java
@@ -3,7 +3,9 @@ package com.codecool.config;
 
 import com.codecool.model.Interest;
 import com.codecool.model.User;
+import com.codecool.model.UserDetail;
 import com.codecool.repository.InterestRepository;
+import com.codecool.repository.UserDetailRepository;
 import com.codecool.security.Role;
 import com.codecool.security.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,10 +32,7 @@ public class DataLoader {
     private UserService userService;
 
     @Autowired
-    public DataLoader(InterestRepository interestRepository, UserService userService) {
-        this.interestRepository = interestRepository;
-        this.userService = userService;
-    }
+    private UserDetailRepository userDetailRepository;
 
     @PostConstruct
     public void loadBasicActivities() {
@@ -47,6 +46,8 @@ public class DataLoader {
         if (userService.getUserByEmail("admin@admin.com").equals(Optional.empty())) {
             User admin = new User("admin@admin.com", "1234");
             userService.create(admin, Role.ADMIN);
+            UserDetail userDetail = new UserDetail(admin);
+            userDetailRepository.save(userDetail);
         }
     }
 }
