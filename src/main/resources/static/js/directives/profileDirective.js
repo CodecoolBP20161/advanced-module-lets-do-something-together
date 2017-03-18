7bdf551d7ff9af140ff8371215356fc19704d72c
DorciM <dora.medgyasszay@gmail.com>
2017-03-18 22:22:54 +0100

    style except index

--- src/main/resources/static/js/directives/profileDirective.js
+++ src/main/resources/static/js/directives/profileDirective.js
@@ -1,10 +1,17 @@
 'use strict';
 
-var actimate = angular.module('actimate',['ngResource']);
+var actimate = angular.module('actimate',['ngResource', 'jcs-autoValidate']);
 actimate.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
+
+actimate.run(function(defaultErrorMessageResolver){
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+            errorMessages['wrongInputType'] = 'Please enter letters only!';
+        });
+    }
+);
 
 actimate.directive('profileController', function() {
     return {
