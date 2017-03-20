0985b3e3cceb6891b35ff7a5bd5d8eec4a54c9ce
cickib <bellakocsis06@gmail.com>
2017-03-20 10:35:29 +0100

    LSD-72 - unnecessary admin related filtering removed

--- src/main/java/com/codecool/controller/AdminController.java
+++ src/main/java/com/codecool/controller/AdminController.java
@@ -1,8 +1,5 @@
 package com.codecool.controller;
 
-import com.codecool.model.Interest;
-import com.codecool.model.event.Event;
-import com.codecool.model.event.Status;
 import com.codecool.repository.EventRepository;
 import com.codecool.repository.InterestRepository;
 import com.codecool.repository.UserEmailRepository;
@@ -14,9 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 @RequestMapping(value = "/admin")
 @Controller
@@ -50,36 +44,7 @@ public class AdminController extends AbstractController {
 
     @RequestMapping(value = "/events", method = RequestMethod.POST)
     public String listEvents(HttpServletRequest request, Model model) throws IOException {
-        List<Event> events;
-        String key = request.getParameter("key").toLowerCase();
-        String filter = request.getParameter("filter").toLowerCase();
-        try {
-            switch (filter) {
-                case "status":
-                    if (key.equals("active")) {
-                        Status status = Status.ACTIVE;
-                        events = eventRepository.findByStatus(status);
-                        model.addAttribute("events", events);
-                    } else {
-                        Status status = Status.PAST;
-                        events = eventRepository.findByStatus(status);
-                        model.addAttribute("events", events);
-                    }
-                    break;
-                case "date":
-                    DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-                    events = eventRepository.findByDate(format.parse(key));
-                    model.addAttribute("events", events);
-                    break;
-                case "activities":
-                    Interest interest = interestRepository.findByActivity(key);
-                    events = eventRepository.findByInterest(interest);
-                    model.addAttribute("events", events);
-                    break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        model.addAttribute("events", eventRepository.findAll());
         return "admin/admin_events";
     }
 
