20df41c6b1374c52ec382da505f9f24b67e7864d
cickib <bellakocsis06@gmail.com>
2017-03-24 12:05:22 +0100

    LSD-135 - DashBoardController logs

--- src/main/java/com/codecool/controller/DashBoardController.java
+++ src/main/java/com/codecool/controller/DashBoardController.java
@@ -2,16 +2,15 @@ package com.codecool.controller;
 
 import com.codecool.model.event.Event;
 import com.codecool.model.event.Status;
-import com.codecool.repository.EventRepository;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
 import java.util.List;
 
 
@@ -19,25 +18,31 @@ import java.util.List;
 @Controller
 public class DashBoardController extends AbstractController {
 
+    private static final Logger logger = LoggerFactory.getLogger(DashBoardController.class);
+
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String dashboard() {
+        logger.info("/u/dashboard route called.");
         return "user_main";
     }
 
     @RequestMapping(value = "/events", method = RequestMethod.GET)
     @ResponseBody
     public String collectEvents() {
+        logger.info("/u/events route called.");
         JSONObject events = new JSONObject();
         try {
             events.put("events", getAllEvents());
+            logger.info("All events collected to dashboard.");
         } catch (JSONException e) {
-            e.printStackTrace();
+            logger.error("{} occurred while collecting events to json: {}", e.getCause(), e.getMessage());
         }
         return events.toString();
     }
 
     private JSONObject getAllEvents() {
         List<Event> events = eventRepository.findByStatus(Status.ACTIVE);
+        logger.debug("{} active events collected.", events.size());
         return eventUtil.createEventsJson(events);
     }
 
