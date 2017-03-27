ba69f23818ca9c57157a617818186b96f9bc783d
cickib <bellakocsis06@gmail.com>
2017-03-27 12:56:00 +0200

    LSD-140 - ContactRepository implemented

---
+++ src/main/java/com/codecool/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.repository;
+
+import com.codecool.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+}
