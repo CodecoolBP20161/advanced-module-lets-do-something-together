72fa3a5906ae1218c6aeb3be6f90732737a79586
lorszil <lorszil@gmail.com>
2017-03-31 15:06:01 +0200

    creating routes and adding controllers

--- src/main/resources/static/js/route.js
+++ src/main/resources/static/js/route.js
@@ -2,20 +2,37 @@
 
 actimate.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: 'index.html'
+        .when('', {
+            templateUrl: 'main.html',
+            controller: 'mainCtrl'
         })
         .when('/u/profile', {
-            templateUrl: 'profile.html'
+            templateUrl: 'profile.html',
+            controller: "loadUserCtrl"
         })
         .when('/u/dashboard', {
-            templateUrl: 'user_main.html',
+            template: 'user_main.html',
             controller: "loadEventCtrl"
         })
+        .when('/u/edit-profile', {
+            templateUrl: 'profile_form.html',
+            controller: 'profileController'
+        })
+        .when('/u/create_event', {
+            templateUrl: 'create_event.html',
+            controller: "saveEventCtrl"
+        })
         .when('/registration', {
-            templateUrl: 'registration.html'
+            template: 'registration.html',
+            controller: "userCtrl"
+        })
+        .when('/login', {
+            templateUrl: 'login_form.html'
+            //controller: "userCtrl"
         })
         .otherwise({
             redirectTo: '/'
         });
 }]);
+
+//actimate.run();
