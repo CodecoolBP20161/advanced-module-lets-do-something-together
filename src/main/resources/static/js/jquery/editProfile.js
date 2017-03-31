a84dc4f2e0b1705835f58cc07c065bb643ec435c
cickib <bellakocsis06@gmail.com>
2017-03-31 14:43:59 +0200

    LSD-146 - disabled submit btn fixed

--- src/main/resources/static/js/jquery/editProfile.js
+++ src/main/resources/static/js/jquery/editProfile.js
@@ -2,5 +2,6 @@ function updateInterests(list) {
     for (var i = 0; i < list.length; i++) {
         var id = "#" + list[i];
         $(id).prop('checked', true);
+        $("input[type='submit']").removeAttr("disabled");
     }
 }
