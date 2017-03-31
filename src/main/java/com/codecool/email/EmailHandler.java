988b7506b3db9bdc2ebb4c8de797c6f396700e3e
cickib <bellakocsis06@gmail.com>
2017-03-31 13:37:24 +0200

    LSD-135 - EmailHandler logs

--- src/main/java/com/codecool/email/EmailHandler.java
+++ src/main/java/com/codecool/email/EmailHandler.java
@@ -6,6 +6,8 @@ import com.codecool.model.UserEmail;
 import com.codecool.repository.ContactRepository;
 import com.codecool.repository.UserEmailRepository;
 import com.codecool.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,7 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class EmailHandler {
+    private static final Logger logger = LoggerFactory.getLogger(EmailHandler.class);
 
     @Autowired
     private UserEmailRepository userEmailRepository;
@@ -27,6 +30,7 @@ public class EmailHandler {
     public ContactRepository contactRepository;
 
     public List<User> checkEmailStatus() {
+        logger.info("New users without welcome email collected to list.");
         return userEmailRepository
                 .findAllByEmailSent(false)
                 .stream()
@@ -35,13 +39,16 @@ public class EmailHandler {
     }
 
     public void updateEmailStatus(List<String> emails) {
+        logger.info("{} user(s)' email(s)' status(es) updated.", emails.size());
         for (String email : emails) {
             try {
                 UserEmail userEmail = userEmailRepository.findByUser(userRepository.findByEmail(email));
                 userEmail.setEmailSent(true);
                 userEmailRepository.save(userEmail);
+                logger.info("Welcome email status set to sent for new user.");
             } catch (NullPointerException e) {
                 e.getMessage();
+                logger.info("Contact email status set to forwarded for new contact.");
                 manageContactEmails(email);
             }
         }
@@ -49,6 +56,7 @@ public class EmailHandler {
 
     private void manageContactEmails(String email) {
         String contactEmail = email.substring(email.indexOf("+") + 1, email.indexOf("@"));
+        logger.info("Update contact email status for '{}'.", contactEmail);
         contactRepository.findAllByForwarded(false)
                 .stream()
                 .filter(contact ->
@@ -58,10 +66,12 @@ public class EmailHandler {
 
 
     public String concatEmailAddress(Contact contact) {
+        logger.info("Contact email address concatenated with app email.");
         return String.format("actimate.app+%s@gmail.com", replaceNonAlphaNumericCharacters(contact.getEmail()));
     }
 
     private String replaceNonAlphaNumericCharacters(String email) {
+        logger.info("Email address special charcters replaced in '{}'.", email);
         return email.replaceAll("\\W", "");
     }
 }
