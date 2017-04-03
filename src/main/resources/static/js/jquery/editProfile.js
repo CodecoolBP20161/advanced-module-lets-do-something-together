57cb3c06495e3453b6de847e3c03d4a01d4287be
cickib <bellakocsis06@gmail.com>
2017-04-03 14:46:22 +0200

    LSD-146 - checking checked checboxes with jquery

--- src/main/resources/static/js/jquery/editProfile.js
+++ src/main/resources/static/js/jquery/editProfile.js
@@ -1,7 +1,24 @@
 function updateInterests(list) {
     for (var i = 0; i < list.length; i++) {
-        var id = "#" + list[i];
+        var id = createSelector(list[i]);
         $(id).prop('checked', true);
         $("input[type='submit']").removeAttr("disabled");
     }
 }
+
+function checkInterests() {
+    var interests = [];
+
+    $('#profileForm *').filter(':checkbox').each(function () {
+        var id = this.id;
+        if ($(createSelector(id)).prop("checked")) {
+            interests.push(id)
+        }
+    });
+
+    return interests;
+}
+
+var createSelector = function (id) {
+    return "#" + id;
+};
