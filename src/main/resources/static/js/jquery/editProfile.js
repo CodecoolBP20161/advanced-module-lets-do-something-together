ec9b5e11fb24e17af31ff1e6578d7c28d7399146
cickib <bellakocsis06@gmail.com>
2017-04-04 10:45:20 +0200

    LSD-146 - js checks if interestList is empty

--- src/main/resources/static/js/jquery/editProfile.js
+++ src/main/resources/static/js/jquery/editProfile.js
@@ -1,8 +1,10 @@
 function updateInterests(list) {
-    for (var i = 0; i < list.length; i++) {
-        var id = createSelector(list[i]);
-        $(id).prop('checked', true);
+    if (list.length > 0) {
         $("input[type='submit']").removeAttr("disabled");
+        for (var i = 0; i < list.length; i++) {
+            var id = createSelector(list[i]);
+            $(id).prop('checked', true);
+        }
     }
 }
 
