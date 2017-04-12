999506bb0b9de78045f036a3564b15724ae8c4d2
lorszil <lorszil@gmail.com>
2017-04-12 14:24:07 +0200

    adding rootscope.on to check the path

--- src/main/resources/static/js/controller/menuController.js
+++ src/main/resources/static/js/controller/menuController.js
@@ -1,7 +1,11 @@
 'use strict';
 
 actimate.controller('menuCtrl', ['$rootScope', '$location', '$scope', function($rootScope, $location, $scope) {
-    $scope.currentPath = $location.path();
-    // console.log("Path: " + $location.path())
+
+    $rootScope.$on("$locationChangeStart", function(event, next, current) {
+        $scope.currentPath = $location.path();
+    });
+
+    console.log("Path: " + $location.path())
 
 }]);
