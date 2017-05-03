5aea29e6a6896ac40f10f7b718d333c3b0b1e56d
berloc <berloc1987@gmail.com>
2017-05-03 13:20:44 +0200

    LSD-angular_refactor created logout method

--- src/main/resources/static/js/controller/menuController.js
+++ src/main/resources/static/js/controller/menuController.js
@@ -1,8 +1,17 @@
 'use strict';
 
-actimate.controller('menuCtrl', ['$rootScope', '$location', '$scope', function($rootScope, $location, $scope) {
+actimate.controller('menuCtrl', ['$rootScope', '$location', '$scope', '$http', function($rootScope, $location, $scope, $http) {
 
     $rootScope.$on("$locationChangeStart", function(event, next, current) {
         $scope.currentPath = $location.path();
     });
+
+    $scope.logout = function() {
+
+        $http.get("/logout")
+            .then(function (response) {
+                window.location.href = "/";
+                console.log("kiskutyafasza logout");
+            });
+    }
 }]);
