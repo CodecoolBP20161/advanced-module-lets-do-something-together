977bc32a5f59821f209e5fc1be60e73faf4c078b
berloc <berloc1987@gmail.com>
2017-05-04 16:43:15 +0200

    LSD-angular_refactor get interests from server instead of hardcoding into html

--- src/main/resources/static/js/controller/eventController.js
+++ src/main/resources/static/js/controller/eventController.js
@@ -7,32 +7,46 @@ actimate.run(function (defaultErrorMessageResolver) {
             });
         })
 
-    .controller('saveEventCtrl', function ($scope, $http) {
+    .controller('eventCtrl', function ($scope, $http) {
         $scope.event = {};
 
-            $('#datetimepicker1').on('dp.change', function (data) {
-                $scope.event.date = moment(data.date).format("DD/MM/YYYY HH:mm a");
-                $scope.$apply();
-            });
+        $('#datetimepicker1').on('dp.change', function (data) {
+            $scope.event.date = moment(data.date).format("DD/MM/YYYY HH:mm a");
+            $scope.$apply();
+        });
 
-            $scope.saveEvent = function () {
+        $scope.saveEvent = function () {
 
-                $http({
-                    method: 'POST',
-                    url: '/u/create_event',
-                    headers: {'Content-Type': 'application/json; charset=UTF-8'},
-                    data: JSON.stringify($scope.event)
+            $http({
+                method: 'POST',
+                url: '/u/create_event',
+                headers: {'Content-Type': 'application/json; charset=UTF-8'},
+                data: JSON.stringify($scope.event)
+            })
+                .then(function (response) {
+                    console.log($scope.event);
+                    console.log(response);
                 })
-                    .then(function (response) {
-                        console.log($scope.event);
-                    })
-            };
+        };
 
-            $scope.$on('gmPlacesAutocomplete::placeChanged', function () {
-                var location = $scope.autocomplete.getPlace().geometry.location;
-                $scope.event.lat = location.lat();
-                $scope.event.lng = location.lng();
-                $scope.event.location = $scope.autocomplete.getPlace().formatted_address;
-                $scope.$apply();
-            });
+        $scope.$on('gmPlacesAutocomplete::placeChanged', function () {
+            var location = $scope.autocomplete.getPlace().geometry.location;
+            $scope.event.lat = location.lat();
+            $scope.event.lng = location.lng();
+            $scope.event.location = $scope.autocomplete.getPlace().formatted_address;
+            $scope.$apply();
+        });
+
+
+        $scope.getInterest = function () {
+
+            $scope.listOfInterests = null;
+
+            $http.get("/interests")
+                .then(function (response) {
+                    $scope.listOfInterests = response.data;
+                })
+        };
+
+        $scope.getInterest();
     });
