9b888d0394f80e1fb0b71bf60b07d8b68f515eb2
berloc <berloc1987@gmail.com>
2017-05-29 22:57:46 +0200

    LSD-angular_refactor reformat code

--- src/main/resources/static/js/controller/dashboardController.js
+++ src/main/resources/static/js/controller/dashboardController.js
@@ -2,14 +2,13 @@
 
 actimate.controller("dashboardCtrl", function ($scope, $http) {
 
-    $scope.getEvents= function () {
+    $scope.getEvents = function () {
 
         $scope.events = null;
 
         $http.get("/u/events")
             .then(function (response) {
                 $scope.events = response.data.events;
-                console.log($scope.events);
             })
     };
 
